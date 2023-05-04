2a6238659a8f005bb76dc48b01bdf6cdea0fe810
Maciej Borkowski <maciej.borkowski@nl.abnamro.com>
2023-05-04 09:56:36 +0200

    change window title

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import rl "github.com/gen2brain/raylib-go/raylib"
 
 func main() {
-	rl.InitWindow(800, 450, "raylib [core] example - basic window")
+	rl.InitWindow(800, 450, "framework")
 	defer rl.CloseWindow()
 	rl.SetTargetFPS(60)
 
@@ -11,7 +11,7 @@ func main() {
 		rl.BeginDrawing()
 
 		rl.ClearBackground(rl.RayWhite)
-		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+		rl.DrawText("framework", 190, 200, 20, rl.LightGray)
 
 		rl.EndDrawing()
 	}
